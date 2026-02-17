a063ecda3cf95e6055be9cd4f8686b4c59cb4e1a
pranjal25r <pranjal25r@gmail.com>
2026-02-17 23:48:44 +0530

    Add solution for Solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,1 +1,13 @@
-c
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX, maxprofit = 0;
+        for(int i=0; i<prices.size(); i++){
+            if(prices[i] < minprice)
+            minprice = prices[i];
+            else
+            maxprofit = max(maxprofit, prices[i] - minprice);
+        }
+    return maxprofit;
+    }
+};
