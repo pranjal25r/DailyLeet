6e2f01f08028a11926dcdfe86ff5ef7c527c4582
pranjal25r <pranjal25r@gmail.com>
2026-02-17 23:40:38 +0530

    Add solution for Solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,14 +1,1 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0, maxsum = INT_MIN;
-        for(int i=0; i<n;i++){
-            sum += nums[i];
-            if(sum>maxsum)
-            maxsum = sum;
-            if(sum < 0)
-            sum = 0;
-        }
-        return maxsum;
-    }
+c
