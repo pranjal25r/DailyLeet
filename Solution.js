48c58029d52c9af9a29235750451dd4f4c3a851f
pranjal25r <pranjal25r@gmail.com>
2026-02-17 23:21:06 +0530

    Delete Solution.js

--- Solution.js
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int reverseBits(int n) {
-        unsigned int num = (unsigned int) n;
-        unsigned int result = n;
-        for(int i = 0;i < 32; i++){
-            result <<= 1;          // shift result left
-            result |= (n & 1);     // take last bit of n
