bb9b01558f66afcb256a326de9755cdb3e2f6aa8
pranjal25r <pranjal25r@gmail.com>
2026-02-19 23:12:53 +0530

    Add solution for 165. Compare Version Numbers
    Med.

--- Compare_Version_Numbers__Med_.cpp
+++ Compare_Version_Numbers__Med_.cpp
@@ -1,10 +1,17 @@
-class Solution {
+class Solution {
 public:
-    int countBinarySubstrings(string s) {
-        int ans = 0;
-        int prev = 0, curr = 1;
-        
-        for(int i=1;i<s.length();i++){
-            if(s[i] == s[i-1])
-            curr++;
+    int countBinarySubstrings(string s) {
+        int ans = 0;
+        int prev = 0, curr = 1;
+        
+        for(int i=1;i<s.length();i++){
+            if(s[i] == s[i-1])
+            curr++;
+
+            else{
+                ans += min(prev,curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
 
