d1a76a4d3b9eb110dd79d3300134768ba21d694b
pranjal25r <pranjal25r@gmail.com>
2026-02-19 23:11:28 +0530

    Add solution for Solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,1 +1,10 @@
-c
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int ans = 0;
+        int prev = 0, curr = 1;
+        
+        for(int i=1;i<s.length();i++){
+            if(s[i] == s[i-1])
+            curr++;
+
