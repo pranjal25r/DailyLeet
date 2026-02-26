d29ef8da19dbf78c3c72b4d31494dd50eafcb63b
pranjal25r <pranjal25r@gmail.com>
2026-02-26 19:30:05 +0530

    Add solution for 493. Reverse Pairs

--- Reverse_Pairs.cpp
+++ Reverse_Pairs.cpp
@@ -1,1 +1,17 @@
-c
+class Solution {
+public:
+
+    void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; 
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
