ad474e32e9991ac8ad2ac8ca7c9a081d8a9c36fc
pranjal25r <pranjal25r@gmail.com>
2026-03-10 16:26:40 +0530

    Add solution for 3129. Find All Possible Stable Binary Arrays I

--- Find_All_Possible_Stable_Binary_Arrays_I.cpp
+++ Find_All_Possible_Stable_Binary_Arrays_I.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class·‌Solution·‌{
 public:
-    const int MOD = 1e9+7;
-    long long dp[201][201][2];
+·‌·‌·‌·‌const·‌int·‌MOD·‌=·‌1e9+7;
+·‌·‌·‌·‌long·‌long·‌dp[201][201][2];
 
-    long long solve(int z, int o, int last, int limit) {
-        if(z==0 && o==0) return 1;
+·‌·‌·‌·‌long·‌long·‌solve(int·‌z,·‌int·‌o,·‌int·‌last,·‌int·‌limit)·‌{
+·‌·‌·‌·‌·‌·‌·‌·‌if(z==0·‌&&·‌o==0)·‌return·‌1;
 
-        if(dp[z][o][last] != -1)
-            return dp[z][o][last];
+·‌·‌·‌·‌·‌·‌·‌·‌if(dp[z][o][last]·‌!=·‌-1)·‌·‌//if·‌we·‌already·‌solved·‌then·‌don't·‌recompute
+·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌dp[z][o][last];·‌
 
-        long long ans = 0;
+·‌·‌·‌·‌·‌·‌·‌·‌long·‌long·‌ans·‌=·‌0;
 
-        if(last == 1) { 
+·‌·‌·‌·‌·‌·‌·‌·‌if(last·‌==·‌1)·‌{·‌
