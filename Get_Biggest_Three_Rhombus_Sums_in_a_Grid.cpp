daa9ddff4a42bf35f91219a4fb050c7e3a2edaa4
Pranjal <pranjal25r@gmail.com>
2026-03-17 19:02:27 +0530

    Add solution for 1878. Get Biggest Three Rhombus Sums in a Grid

--- Get_Biggest_Three_Rhombus_Sums_in_a_Grid.cpp
+++ Get_Biggest_Three_Rhombus_Sums_in_a_Grid.cpp
@@ -1,14 +1,1 @@
-class Solution {
-public:
-    vector<int> getBiggestThree(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        set<int> sums;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-
-                // size 0 rhombus (single cell)
-                sums.insert(grid[i][j]);
-
+c
