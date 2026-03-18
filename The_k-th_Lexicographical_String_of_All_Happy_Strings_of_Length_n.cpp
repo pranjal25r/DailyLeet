746f38b456b8d6bc2d9b40c862273e853e801e87
Pranjal <pranjal25r@gmail.com>
2026-03-18 17:56:14 +0530

    Add solution for 1415. The k-th Lexicographical String of All Happy Strings of Length n

--- The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
+++ The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
@@ -1,1 +1,14 @@
-c
+class Solution {
+public:
+    string ans = "";
+    int count = 0;
+
+    void dfs(string &curr, int n, int k) {
+        if(curr.size() == n) {
+            count++;
+            if(count == k) {
+                ans = curr;
+            }
+            return;
+        }
+
